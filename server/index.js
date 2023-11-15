947e07959ba75780ff44bd97c306f0b3f271d907
Neha Kumari <76866948+Nehakumari02@users.noreply.github.com>
2023-11-15 16:30:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const todoModel=require('./model/todolist')
 const app=express()
 app.use(cors({
     
-    origin: 'http://localhost:5173',
+    origin: 'https://todo-list2-six.vercel.app/',
     methods: ["GET", "POST","PUT" ,"DELETE"],
     credentials: true,
 
